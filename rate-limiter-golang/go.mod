93186e7cdc1c672148eea245dc39de9ff8801e73
Ninad Bhangui <b.ninad50@gmail.com>
2023-11-28 10:18:45 +0530

    init

---
+++ rate-limiter-golang/go.mod
@@ -0,0 +1,3 @@
+module rate-limiter-golang
+
+go 1.21
---
+++ rate-limiter-golang/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type TokenBucket struct {
+	tokenCount   int
+	bucketSize   int
+	refillRate   int
+	internalFunc func()
+}
+
+func (t *TokenBucket) CallInternalFunc() error {
+	fmt.Println("debug: calling internal")
+	if t.revokeToken() == true {
+		fmt.Println("debug: revoked token successfully")
+		t.internalFunc()
+		return nil
+	} else {
+    fmt.Println("debug: rate limit exceeded")
+		return errors.New("Rate limit exceeded")
+	}
+}
+func (t *TokenBucket) StartFill() {
+	for range time.Tick(time.Second * 1) {
+		t.addToken()
+	}
+}
+
+func (t *TokenBucket) addToken() {
+	if t.tokenCount < t.bucketSize {
+		newBucketSize := t.tokenCount + t.refillRate
+		if newBucketSize <= t.bucketSize {
+			t.tokenCount = newBucketSize
+		} else {
+			t.tokenCount = t.bucketSize
+		}
+	}
+}
+
+func (t *TokenBucket) revokeToken() bool {
+  fmt.Println("debug: current token count: ", t.tokenCount)
+	if t.tokenCount > 0 {
+		t.tokenCount -= 1
+		return true
+	}
+	return false
+}
+
+func createTokenBucket(bucketSize int, refillRate int, internalFunc func()) *TokenBucket {
+	limiter := &TokenBucket{1, bucketSize, refillRate, internalFunc}
+	go limiter.StartFill()
+	return limiter
+}
+
+func main() {
+	limiter := createTokenBucket(3, 1, func() { fmt.Println("internal function invoked") })
+	limiter.CallInternalFunc()
+	limiter.CallInternalFunc()
+  fmt.Println("debug: Sleeping...")
+  time.Sleep(2*time.Second)
+	limiter.CallInternalFunc()
+	limiter.CallInternalFunc()
+	limiter.CallInternalFunc()
+}
